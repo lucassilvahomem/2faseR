83457017ec542ddd898e4260b56e348f998aefc0
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2025-04-03 21:52:51 -0300

    Descrição do commit

--- inputs/src/App.jsx
+++ inputs/src/App.jsx
@@ -4,10 +4,18 @@ import './App.css'
 function App() {
   const[inputUsername, setinputUsername] = useState('')
   const[inputSenha, setInputSenha] = useState('')
+  const[erro, setErro] = useState(true)
 
 function efetuarLogin(){
-  alert(inputUsername)
-  alert(inputSenha)
+  // alert(inputUsername)
+  // alert(inputSenha)
+  if(inputUsername == 'lucas' && inputSenha == '69'){
+    alert("pode entrar")
+    setErro(false)
+  }else{
+    // alert("não pode entrar")
+    setErro(true)
+  }
 }
 
   return (
@@ -17,11 +25,14 @@ function efetuarLogin(){
       <input type="text"
        value={inputUsername} 
        onChange={(event) => setinputUsername(event.target.value)}/>
+
        <label htmlFor="">Senha</label>
        <input type="Number"
        value={inputSenha} 
        onChange={(event) => setInputSenha(event.target.value)}/>
        {/* {inputUsername} */}
+       {erro && <span className='meg-error'>Credencias invalidas</span>}
+       {/* <span className='Men-error'> Credencias invalido </span> */}
        <button onClick={efetuarLogin}>Login</button>
     </div>
   )
