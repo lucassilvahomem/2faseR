43a1df764418c72b14142f5a396e0ed99b949649
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2025-04-09 21:57:30 -0300

    Descrição do commit

---
+++ exercicios/src/conponents/Paineldeoraçoes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Paineldeoraçoes() {
+  return (
+    <div className='container-painel'>
+        <h1>Orações devs</h1>
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+    </div>
+  )
+}
+
+export default Paineldeoraçoes
