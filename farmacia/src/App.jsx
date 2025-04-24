d8f481e21813d7c3d0014acda6b2d48ca322581e
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2025-04-23 22:07:02 -0300

    Descrição do commit

---
+++ farmacia/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <div className='container-topo'>
+      <img src="/img/farmarcia-logo-pq.PNG.png" alt="" />
+      </div>
+      <div className='container-center'>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
+        <div className='item'>4</div>
+        <div className='item'>5</div>
+      </div>
+      <div className='container-down'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
